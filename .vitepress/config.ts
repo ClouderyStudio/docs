7337e0959302bc323d60d927b16a0fceb2a32f3c
JustQiyi <justqiyi@qq.com>
2025-08-05 03:45:19 +0800

    docs(team): 团队页面小调整

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -120,7 +120,7 @@ export default defineConfig({
       { text: '服务', link: '/serve/', activeMatch: '/serve/'},
       { text: '笔墨星河', link: '/bmdocs/', activeMatch: '/bmdocs/'},
       { text: '团队', link: '/team' },
-      { text: '导航', link: '/nav' },
+      { text: '导航', link: '/nav/' },
     ],
     
     sidebar: {
