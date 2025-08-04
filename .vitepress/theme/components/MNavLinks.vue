d086719e33d71a53d41f6659950e8964b6aeb022
JustQiyi <justqiyi@qq.com>
2025-08-05 03:34:54 +0800

    perf: 大写有问题 fix: webkit错误

--- .vitepress/theme/components/MNavLinks.vue
+++ .vitepress/theme/components/MNavLinks.vue
@@ -2,7 +2,7 @@
 import { computed } from 'vue'
 import { slugify } from '@mdit-vue/shared'
 
-import MNavLink from './MNavLink.vue'
+import MNavlink from './MNavlink.vue'
 import type { NavLink } from '../../utils/types'
 
 const props = defineProps<{
@@ -22,7 +22,7 @@ const formatTitle = computed(() => {
     <a class="header-anchor" :href="`#${formatTitle}`" aria-hidden="true"></a>
   </h2>
   <div class="m-nav-links">
-    <MNavLink v-for="item in items" :noIcon="noIcon" v-bind="item" />
+    <MNavlink v-for="item in items" :noIcon="noIcon" v-bind="item" />
   </div>
 </template>
 
--- nav/index.md
+++ nav/index.md
@@ -14,4 +14,4 @@ import { NAV_DATA } from '/_data/navlinks'
 </script>
 
 # 导航
-<MNavLinks v-for="{title, items} in NAV_DATA" :title="title" :items="items"/>
+<MNavlinks v-for="{title, items} in NAV_DATA" :title="title" :items="items"/>
