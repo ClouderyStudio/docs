0ba2344e755d19a329a5d9c3bfb9c8aa7c7b8631
云竹 <zhangye_360_198@163.com>
2025-08-05 03:25:03 +0800

    feat: 初步修改nav

--- _data/navlinks.ts
+++ _data/navlinks.ts
@@ -2,83 +2,235 @@ import type { NavData } from "../.vitepress/utils/types"
 
 export const NAV_DATA: NavData[] = [
   {
-    title: '常用工具',
+    title: '云术团队|Cloudery',
     items: [
       {
-        icon: 'https://caniuse.com/img/favicon-128.png',
-        title: 'Can I use',
-        badge: {
-          text: '茂神牛批',
-          type: 'info',
-        },
-        desc: '前端 API 兼容性查询',
-        link: 'https://caniuse.com'
+        title: '云术工作室 | Cloudery Studio Official Site',
+        link: 'https://cdery.com'
       },
       {
-        icon: 'https://tinypng.com/images/apple-touch-icon.png',
-        title: 'TinyPNG',
-        badge: {
-          text: '茂神牛批',
-          type: 'tip',
-        },
-        desc: '在线图片压缩工具',
-        link: 'https://tinypng.com'
+        title: '生存战争论坛盒子',
+        link: 'https://scbbs.top'
       },
       {
-        icon: 'https://devtool.tech/logo.svg',
-        title: '开发者武器库',
-        badge: {
-          text: '茂神牛批',
-          type: 'warning',
-        },
-        desc: '开发者武器库，做开发者最专业最好用的专业工具箱',
-        link: 'https://devtool.tech'
+        title: '云术博客 - 新时代的伟岸开拓者',
+        link: 'https://blog.cdery.com'
       },
       {
-        icon: 'https://tool.lu/favicon.ico',
-        title: '在线工具',
-        badge: {
-          text: '茂神牛批',
-          type: 'danger',
-        },
-        desc: '开发人员的工具箱',
-        link: 'https://tool.lu'
+        title: 'MC竹像素',
+        link: 'https://www.zhuxs.cn'
       },
       {
-        icon: '/icons/json-cn.ico',
-        title: 'Json 中文网',
-        badge: {
-          text: '请给茂茂点点小星星哦',
-          type: 'info',
-        },
-        desc: 'JSON 在线解析及格式化验证',
-        link: 'https://www.json.cn'
+        title: '云术文档站',
+        link: 'https://doc.cdery.com'
+      },
+      {
+        title: '云术生存战争服务器官网',
+        link: 'https://sc.cdery.com'
+      },
+      {
+        title: '云术小游戏平台',
+        link: 'https://game.x8.pw'
+      },
+      {
+        title: '人生重开模拟器原版',
+        link: 'https://relife.x8.pw'
+      },
+      {
+        title: '一些小游戏网站',
+        link: 'https://fun.x8.pw/sgames/'
+      },
+      {
+        title: 'MC在线网页版',
+        link: 'https://fun.x8.pw/webmc/'
+      },
+      {
+        title: '❤️站❤️长❤️推❤️荐❤️',
+        link: 'https://fun.x8.pw/r18/'
+      },
+      {
+        title: 'ClouderyAList',
+        link: 'https://a.cdery.com'
+      },
+      {
+        title: '人生重开模拟器破解版',
+        link: 'https://relifevip.x8.pw'
+      },
+      {
+        title: '留言板',
+        link: 'https://x8.pw/?c=guestbook'
       }
     ]
   },
   {
-    title: 'AI 导航',
+    title: '生存战争',
     items: [
       {
-        icon: '/icons/chatgpt.png',
-        title: 'ChatGPT（最强）',
-        link: 'https://chat.openai.com/chat'
+        title: '生存战争中文社区',
+        link: 'https://www.schub.top/'
       },
       {
-        icon: 'https://www.notion.so/images/logo-ios.png',
-        title: 'Notion AI（笔记）',
-        link: 'https://www.notion.so'
+        title: '生存战争盒子',
+        link: 'https://schz.top'
       },
       {
-        icon: 'https://www.midjourney.com/apple-touch-icon.png',
-        title: 'Midjourney（绘画）',
-        link: 'https://www.midjourney.com'
-      },
-      {
-        icon: 'https://global-uploads.webflow.com/59deb588800ae30001ec19c9/5d4891e0e260e3c1bc37b100_beautiful%20ai%20favicon%20%20blue%20square.png',
-        title: 'Beautiful.ai（PPT）',
-        link: 'https://www.beautiful.ai'
+        title: '生存战争导航栏网(旧)',
+        link: 'https://scweb.2s.ink'
       }
+    ]
+  },
+  {
+    title: '网路开发',
+    items: [
+      {
+        title: '111完美邮箱',
+        link: 'https://mail.111.com/'
+      },
+      {
+        title: '失控的防御系统-日出CDN',
+        link: 'https://www.scdn.io/'
+      },
+      {
+        title: '西部数码',
+        link: 'https://www.west.cn/'
+      },
+      {
+        title: '蓝奏·云存储',
+        link: 'https://www.lanzou.com/'
+      },
+      {
+        title: 'Cloudflare',
+        link: 'https://www.cloudflare.com/'
+      },
+      {
+        title: 'ImgURL免费图床',
+        link: 'https://www.imgurl.org/'
+      },
+      {
+        title: 'IPRSS',
+        link: 'https://ip.rss.ink/'
+      },
+      {
+        title: '腾讯文档',
+        link: 'https://docs.qq.com/'
+      },
+      {
+        title: 'ITDOG',
+        link: 'https://www.itdog.cn/'
+      },
+      {
+        title: 'GitHub',
+        link: 'https://github.com'
+      },
+      {
+        title: 'QQ邮箱',
+        link: 'https://mail.qq.com/'
+      },
+      {
+        title: '必应站长工具箱',
+        link: 'https://www.bing.com/webmasters/'
+      },
+      {
+        title: '123网盘',
+        link: 'https://www.123pan.com/'
+      }
+    ]
+  },
+  {
+    title: '内网穿透 Frp',
+    items: [
+      {
+        title: 'ChmlFrp',
+        link: 'https://www.chmlfrp.cn/'
+      },
+      {
+        title: 'Sakura Frp',
+        link: 'https://www.natfrp.com/'
+      },
+      {
+        title: 'OpenFrp',
+        link: 'https://www.openfrp.net/'
+      },
+      {
+        title: '木韩内网穿透',
+        link: 'https://muhanfrp.cn/'
+      },
+      {
+        title: 'LocyanFrp',
+        link: 'https://www.locyanfrp.cn/'
+      },
+      {
+        title: 'ME Frp',
+        link: 'https://www.mefrp.com/'
+      },
+      {
+        title: '干草Frp',
+        link: 'https://www.hayfrp.org/'
+      },
+      {
+        title: 'MossFrp',
+        link: 'https://www.mossfrp.top/'
+      },
+      {
+        title: 'StarryFrp',
+        link: 'https://frp.starryfrp.com/'
+      },
+      {
+        title: 'DPFRP',
+        link: 'https://frpc.cc/'
+      },
+      {
+        title: '内网穿透联盟',
+        link: 'https://xn--v6qw21h0gd43u.xn--fiqs8s/'
+      }
+    ]
+  },
+  {
+    title: '二级域名',
+    items: [
+      {
+        title: '灵空二级域名分发 - 域名最全的二级域名分发站',
+        link: 'https://lk.stcpic.cn'
+      },
+      {
+        title: '辽阔域名 · 公益分发',
+        link: 'https://lkdns.org'
+      }
+    ]
+  },
+  {
+    title: 'AI',
+    items: [
+      {
+        title: 'Kimi.ai - 会推理解析，能深度思考的AI助手',
+        link: 'https://kimi.ai'
+      },
+      {
+        title: 'deepseek',
+        link: 'https://www.deepseek.com'
+      },
+      {
+        title: '文心一言',
+        link: 'https://yiyan.baidu.com/'
+      },
+      {
+        title: 'ChatGPT镜像',
+        link: 'https://chatai.lra.cn/'
+      }
+    ]
+  },
+  {
+    title: '其他',
+    items: [
+      {
+        title: 'NoBook化学实验模拟',
+        link: 'https://hx.nobook.com/'
+      }
+    ]
+  },
+  {
+    title: '友链',
+    items: [
     ]
   }
 ]
