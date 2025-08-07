421ad70a78d1b6e3546ca0b53e316bb6ae40efc9
云竹 <zhangye_360_198@163.com>
2025-08-07 23:17:34 +0800

    feat: 使用pagefind

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,6 +3,7 @@ import renderPermaLink from './render-perma-link'
 import MarkDownItCustomAnchor from './markdown-it-custom-anchor'
 import { nav } from './configs'
 import { sidebar } from './configs'
+import { pagefindPlugin } from 'vitepress-plugin-pagefind'
 
 const ogDescription = '云术工作室文档站'
 const ogImage = 'https://vitejs.dev/og-image.png'
@@ -49,63 +50,15 @@ export default defineConfig({
     root: { label: '简体中文' },
   },
 
+  vite:{
+    plugins:[pagefindPlugin()],
+  },
+
   themeConfig: {
     logo: '/logo.svg',
 
     outline: {
       label: '本页目录'
-    },
-
-    search: {
-      provider: 'algolia',
-      options: {
-        appId: '3YZHUFB0YW',
-        apiKey: 'f507a2c72af2a8e3e22f9646dc3682a2',
-        indexName: 'doc_cldery_com_3yzhufb0yw_pages',
-        locales: {
-          root: {
-            placeholder: '搜索文档',
-            translations: {
-              button: {
-                buttonText: '搜索文档',
-                buttonAriaLabel: '搜索文档'
-              },
-              modal: {
-                searchBox: {
-                  resetButtonTitle: '清除查询条件',
-                  resetButtonAriaLabel: '清除查询条件',
-                  cancelButtonText: '取消',
-                  cancelButtonAriaLabel: '取消'
-                },
-                startScreen: {
-                  recentSearchesTitle: '搜索历史',
-                  noRecentSearchesText: '没有搜索历史',
-                  saveRecentSearchButtonTitle: '保存至搜索历史',
-                  removeRecentSearchButtonTitle: '从搜索历史中移除',
-                  favoriteSearchesTitle: '收藏',
-                  removeFavoriteSearchButtonTitle: '从收藏中移除'
-                },
-                errorScreen: {
-                  titleText: '无法获取结果',
-                  helpText: '你可能需要检查你的网络连接'
-                },
-                footer: {
-                  selectText: '选择',
-                  navigateText: '切换',
-                  closeText: '关闭',
-                  searchByText: '搜索提供者'
-                },
-                noResultsScreen: {
-                  noResultsText: '无法找到相关结果',
-                  suggestedQueryText: '你可以尝试查询',
-                  reportMissingResultsText: '你认为该查询应该有结果？',
-                  reportMissingResultsLinkText: '点击反馈'
-                },
-              },
-            },
-          },
-        },
-      },
     },
 
     socialLinks: [
