01764f3ea77a2b9735bbbdac083a88a6e0775d11
云竹 <zhangye_360_198@163.com>
2025-08-10 04:51:53 +0800

    feat: 增长公告自动关闭时常

--- .vitepress/theme/components/Notice.vue
+++ .vitepress/theme/components/Notice.vue
@@ -6,7 +6,7 @@ function closetz() {
   visible.value = false
 }
 
-setTimeout(function () { visible.value = false }, 5000);
+setTimeout(function () { visible.value = false }, 15000);
 </script>
 
 <template>
